8a44b5836331eacd80ee79dca4397b4dd20659e6
Sean-Mc-Mahon <sean.mcmahon87@gmail.com>
2020-12-10 09:42:45 +0000

    modal function added

--- static/js/script.js
+++ static/js/script.js
@@ -6,33 +6,34 @@ $(document).ready(function(){
         $('.tabs').tabs();
         $('.parallax').parallax();
         $('.materialboxed').materialbox();
+        $('.modal').modal();
         
         //code taken from Materialize Form Validation lesson by Tim Nelson as part of Code Institute Data Centric Development Module: https://courses.codeinstitute.net/courses/course-v1:CodeInstitute+DCP101+2017_T3/courseware/196c000dd670458cafc7b2dc9d4a8245/f2ad3c6775ce4890a53e62de35245c0a/?child=first
         validateMaterializeSelect();
-    function validateMaterializeSelect() {
-        let classValid = { "border-bottom": "1px solid #4caf50", "box-shadow": "0 1px 0 0 #4caf50" };
-        let classInvalid = { "border-bottom": "1px solid #f44336", "box-shadow": "0 1px 0 0 #f44336" };
-        if ($("select.validate").prop("required")) {
-            $("select.validate").css({ "display": "block", "height": "0", "padding": "0", "width": "0", "position": "absolute" });
-        }
-        $(".select-wrapper input.select-dropdown").on("focusin", function () {
-            $(this).parent(".select-wrapper").on("change", function () {
-                if ($(this).children("ul").children("li.selected:not(.disabled)").on("click", function () { })) {
-                    $(this).children("input").css(classValid);
+            function validateMaterializeSelect() {
+                let classValid = { "border-bottom": "1px solid #4caf50", "box-shadow": "0 1px 0 0 #4caf50" };
+                let classInvalid = { "border-bottom": "1px solid #f44336", "box-shadow": "0 1px 0 0 #f44336" };
+                if ($("select.validate").prop("required")) {
+                    $("select.validate").css({ "display": "block", "height": "0", "padding": "0", "width": "0", "position": "absolute" });
                 }
-            });
-        }).on("click", function () {
-            if ($(this).parent(".select-wrapper").children("ul").children("li.selected:not(.disabled)").css("background-color") === "rgba(0, 0, 0, 0.03)") {
-                $(this).parent(".select-wrapper").children("input").css(classValid);
-            } else {
-                $(".select-wrapper input.select-dropdown").on("focusout", function () {
-                    if ($(this).parent(".select-wrapper").children("select").prop("required")) {
-                        if ($(this).css("border-bottom") != "1px solid rgb(76, 175, 80)") {
-                            $(this).parent(".select-wrapper").children("input").css(classInvalid);
+                $(".select-wrapper input.select-dropdown").on("focusin", function () {
+                    $(this).parent(".select-wrapper").on("change", function () {
+                        if ($(this).children("ul").children("li.selected:not(.disabled)").on("click", function () { })) {
+                            $(this).children("input").css(classValid);
                         }
+                    });
+                }).on("click", function () {
+                    if ($(this).parent(".select-wrapper").children("ul").children("li.selected:not(.disabled)").css("background-color") === "rgba(0, 0, 0, 0.03)") {
+                        $(this).parent(".select-wrapper").children("input").css(classValid);
+                    } else {
+                        $(".select-wrapper input.select-dropdown").on("focusout", function () {
+                            if ($(this).parent(".select-wrapper").children("select").prop("required")) {
+                                if ($(this).css("border-bottom") != "1px solid rgb(76, 175, 80)") {
+                                    $(this).parent(".select-wrapper").children("input").css(classInvalid);
+                                }
+                            }
+                        });
                     }
                 });
             }
         });
-    }
-    });
